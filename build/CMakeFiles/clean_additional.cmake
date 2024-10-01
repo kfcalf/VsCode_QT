b6a7e7a6302b520e7553e5ace9084c567a27be9a
Kfcalf <sunboywfy@live.cn>
2024-10-01 09:42:42 +0800

    first commit

---
+++ build/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,44 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed_Debug.txt"
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed_RelWithDebInfo.txt"
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed_Release.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache_Debug.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache_RelWithDebInfo.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache_Release.txt"
+  "Test6_autogen"
+  )
+endif()
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Release")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed_Debug.txt"
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed_RelWithDebInfo.txt"
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed_Release.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache_Debug.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache_RelWithDebInfo.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache_Release.txt"
+  "Test6_autogen"
+  )
+endif()
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "RelWithDebInfo")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed_Debug.txt"
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed_RelWithDebInfo.txt"
+  "CMakeFiles\\Test6_autogen.dir\\AutogenUsed_Release.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache_Debug.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache_RelWithDebInfo.txt"
+  "CMakeFiles\\Test6_autogen.dir\\ParseCache_Release.txt"
+  "Test6_autogen"
+  )
+endif()
